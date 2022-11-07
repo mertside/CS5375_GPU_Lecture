249c15c990dcac019248499a1a6129031ff7c530
Mert Şide <mertside@gmail.com>
2022-11-07 16:19:14 -0600

    Update submit_job.sh

--- Lecture/submit_job.sh
+++ Lecture/submit_job.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
-#SBATCH --job-name=CS5375_GPU_Lecture
-#SBATCH --output=%x.%j.o
-#SBATCH --error=%x.%j.e
+#SBATCH --job-name=CS5375_eraider_ID
+#SBATCH --output=%j.%N.out
+#SBATCH --error=%j.%N.err
 #SBATCH --partition=matador
 #SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --gpus-per-node=1
+#SBATCH --account= cs5375
+#SBATCH --reservation= cs5375_gpu
 
 nvcc add_v5.cu -o add_v5.exe
-./add_v5.exe
+nvprof ./add_v5.exe
