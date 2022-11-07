c721c1d967cd9a44d40997e34646da8f4aec3332
Mert Şide <mertside@gmail.com>
2022-11-07 17:47:49 -0600

    Update submit_job.sh

--- Lecture/submit_job.sh
+++ Lecture/submit_job.sh
@@ -6,8 +6,10 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gpus-per-node=1
-#SBATCH --account= cs5375
-#SBATCH --reservation= cs5375_gpu
+#SBATCH --account=cs5375
+#SBATCH --reservation=cs5375_gpu
+
+module load gcc cuda
 
 nvcc add_v5.cu -o add_v5.exe
 nvprof ./add_v5.exe
